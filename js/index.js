a8e8069c3b08e671b4db28a3483196be2f2c962a
Sahir Nambiar <sahirnambiar@gmail.com>
2018-10-23 03:43:45 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
